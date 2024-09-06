6fec628f098ed3ac7c44ebb5cfd20681eaaeac93
Fauzudheen Abdul Hameed <fauzudheen1@gmail.com>
2024-09-06 21:28:18 +0530

    feat(k8s): Set up local Kubernetes environment with deployments, stateful sets, and ingress ^

--- server/user_service/user_service/settings.py
+++ server/user_service/user_service/settings.py
@@ -24,7 +24,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
