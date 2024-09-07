539241b9ae666052808a63108c1ffca4b42d683f
Fauzudheen Abdul Hameed <fauzudheen1@gmail.com>
2024-09-07 13:55:31 +0530

    chore: update Gateway URL for cluster environment compatibility

--- client/src/components/const/urls.jsx
+++ client/src/components/const/urls.jsx
@@ -1,3 +1,3 @@
 // export const GatewayUrl = "http://127.0.0.1:8000/"
 // export const GatewayUrl = "http://localhost:80/"
-export const GatewayUrl = import.meta.env.NEXT_PUBLIC_GATEWAY_URL;
+export const GatewayUrl = import.meta.env.VITE_GATEWAY_URL;
