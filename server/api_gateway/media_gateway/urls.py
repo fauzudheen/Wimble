611356af9413c33493b5826bd18f651e8ad22363
Fauzudheen Abdul Hameed <fauzudheen1@gmail.com>
2024-09-09 10:19:55 +0530

    fix: fix media src in frontend for communities in admin side

--- server/api_gateway/media_gateway/urls.py
+++ server/api_gateway/media_gateway/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
 from . import views
+import os
+
+DOMAIN = os.getenv('DOMAIN') 
 
 urlpatterns = [
     re_path(r'^media/.*$', views.MediaView.as_view()),
-    re_path(r'^http://127.0.0.1:8000/media/.*$', views.MediaView.as_view()),
+    # re_path(r'^http://127.0.0.1:8000/media/.*$', views.MediaView.as_view()),
+    re_path(rf'^http://{DOMAIN}/media/.*$', views.MediaView.as_view()),   
 ]
